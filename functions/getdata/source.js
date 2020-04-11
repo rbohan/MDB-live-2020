0601c9a2627f56e2c22d15df8c70cfe294092631
rbohan <rbohan@users.noreply.github.com>
2020-04-11 14:49:17 +0100

    switching to async/await

--- functions/getdata/source.js
+++ functions/getdata/source.js
@@ -1,27 +1,24 @@
-exports = function(){
-  console.log(`Updating billing data`);
-
-  const org =      context.values.get('orgid');
-  const username = context.values.get('publicKey');
-  const password = context.values.get('privateKey');
-
-  const scheme = `https`;
-  const host = `cloud.mongodb.com`;
-  const path = `/api/atlas/v1.0/orgs/${org}/invoices/pending`;
+exports = async function(){
+  try {
+    const org =      context.values.get(`orgid`);
+    const username = context.values.get(`publicKey`);
+    const password = context.values.get(`privateKey`);
   
-  const collection = context.services.get("mongodb-atlas").db("billing").collection("billingdata");
-  
-  return context.http
-    .get({ digestAuth: true, scheme: scheme, host: host, username: username, password: password, path: path })
-    .then(response => {
-      const body = JSON.parse(response.body.text());
-      if (body.error) {
-        console.error(`Error ${body.error}: '${body.detail}'`);
-      } else {
-        const doc = body;
-        console.log(`getBillingData: Upserting new doc with '${doc.lineItems.length}' lineItems`);
-        return collection.updateOne({ "id": doc.id }, doc, { "upsert": true });
-      }
-    })
-    .catch(err => console.error(`getBillingData: Failed to insert billing doc: ${err}`));
+    const scheme = `https`;
+    const host = `cloud.mongodb.com`;
+    const path = `/api/atlas/v1.0/orgs/${org}/invoices/pending`;
+    
+    const collection = context.services.get(`mongodb-atlas`).db(`billing`).collection(`billingdata`);
+    
+    console.log('getdata: calling the billing API');
+
+    const response = await context.http.get({ digestAuth: true, scheme: scheme, host: host, username: username, password: password, path: path })
+    const doc = await JSON.parse(response.body.text());
+    await collection.updateOne({ "id": doc.id }, doc, { "upsert": true });
+
+    console.log('getdata success!');
+  }
+  catch (err) {
+    console.log(`getdata failed: ${err}`);
+  }
 };
