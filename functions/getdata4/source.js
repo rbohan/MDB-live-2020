3f1bfe81af409f99a6b2435bfac05d0030ce0b7f
rbohan <rbohan@users.noreply.github.com>
2020-04-12 12:04:15 +0100

    fixed bug re date calculation

--- functions/getdata4/source.js
+++ functions/getdata4/source.js
@@ -4,8 +4,14 @@ exports = async function(){
     const username = context.values.get(`publicKey`);
     const password = context.values.get(`privateKey`);
 
+    // find the last date in our materialized output (so we know where we are)
+    const collection = context.services.get(`mongodb-atlas`).db(`billing`).collection(`details`);
+    const dates = await collection.find({},{"date":1, "_id":0}).sort({"date": -1}).limit(1).toArray();
+    const date = (dates.length && (dates[0].date instanceof Date) ? dates[0].date : undefined);
+    console.log(`getdata4: date filter = ${date}`);
+
     await callAPIs(org, username, password);
-    await processData();
+    await processData(date);
 
     console.log(`getdata4: success!`);
   }
@@ -31,8 +37,6 @@ callBillingAPIs = async function(org, username, password)
   const host = `cloud.mongodb.com`;
   const path = `/api/atlas/v1.0/orgs/${org}/invoices`;
   
-  const collection = context.services.get(`mongodb-atlas`).db(`billing`).collection(`billingdata`);
-
   const response = await context.http.get({ digestAuth: true, scheme: scheme, host: host, username: username, password: password, path: path });
   const body = await JSON.parse(response.body.text());
   let promises = [];
@@ -90,23 +94,20 @@ callProjectAPI = async function(org, username, password)
   return Promise.all(promises);
 }
 
-processData = async function()
+processData = async function(date)
 {
   console.log(`getdata4: processing data`);
   
-  const details = context.services.get(`mongodb-atlas`).db(`billing`).collection(`details`);
-  const billingdata = context.services.get(`mongodb-atlas`).db(`billing`).collection(`billingdata`);
-
-  const dates = await details.find({},{"date":1, "_id":0}).sort({"date": -1}).limit(1).toArray();
+  const collection = context.services.get(`mongodb-atlas`).db(`billing`).collection(`billingdata`);
 
   let pipeline = [];
   
   // quick filter to avoid processing older invoices
-  // (anything where the endData is a month ago or later)
-  if (dates.length && dates[0].date) {
-    const date = new Date(dates[0].date - 1000 * 3600 * 24 * 31);
-    const strdate = JSON.stringify(date);
-    pipeline.push({ "$match": { "endDate": { "$gte": strdate }}});
+  // (anything where the endData is more recent than
+  // a month prior to the last date we've processed)
+  if (date instanceof Date) {
+    const startfrom = new Date(date - 1000 * 3600 * 24 * 31);
+    pipeline.push({ "$match": { "endDate": { "$gte": JSON.stringify(startfrom) }}});
   }
 
   pipeline.push({ "$lookup": {
@@ -117,16 +118,16 @@ processData = async function()
   }});
   pipeline.push({ "$unwind": { "path": "$orgdata", "preserveNullAndEmptyArrays": true }});
 
-  pipeline.push({ "$unwind": { "path": "$lineItems", "includeArrayIndex": "index", "preserveNullAndEmptyArrays": true }});
+  pipeline.push({ "$unwind": { "path": "$lineItems", "preserveNullAndEmptyArrays": true }});
   pipeline.push({ "$addFields": {
     "date": { "$dateFromString": { dateString: "$lineItems.startDate" }},
     "datetime": { "$split": ["$lineItems.startDate", "T"]}
   }});
 
-  // only process new data
+  // only process the new data
   // (where the date is greater than the last one we've processed)
-  if (dates.length && dates[0].date) {
-    pipeline.push({ "$match": { "date": { "$gt": dates[0].date }}});
+  if (date instanceof Date) {
+    pipeline.push({ "$match": { "date": { "$gt": date }}});
   }
 
   pipeline.push({ "$lookup": {
@@ -138,18 +139,17 @@ processData = async function()
   pipeline.push({ "$unwind": { "path": "$projectdata", "preserveNullAndEmptyArrays": true }});
 
   pipeline.push({ "$project": {
-    "_id": { "id": "$_id", "index": "$index" },
+    "_id": 0,
     "org": { "id": "$orgId", "name": { "$ifNull": ["$orgdata.name", "$orgId" ]} },
     "project": { "id": "$lineItems.groupId", "name": { "$ifNull": ["$projectdata.name", "$lineItems.groupId" ]} },
     "cluster": { "$ifNull": ["$lineItems.clusterName", "--n/a--" ]},
     "sku": "$lineItems.sku",
     "cost": { "$toDecimal": { "$divide": [ "$lineItems.totalPriceCents", 100 ]}},
     "date": 1,
-    "datetime": 1,
-    // "details": "$lineItems"
+    "datetime": 1
   }});
 
   pipeline.push({ "$merge": { "into": "details" }});
 
-  return billingdata.aggregate(pipeline).toArray();
+  return collection.aggregate(pipeline).toArray();
 };
